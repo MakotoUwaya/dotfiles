96eb7e8189a08cb7e27ec139f2d70a44fe8d8e90
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2025-12-24 17:05:43 +0900

    ➕ Add plugin gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,46 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    signs = {
+      add          = { text = "┃" },
+      change       = { text = "┃" },
+      delete       = { text = "_" },
+      topdelete    = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked    = { text = "┆" },
+    },
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "eol", -- 行末に表示
+      delay = 500,           -- 0.5秒待機して表示
+    },
+
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      map("n", "]c", function()
+        if vim.wo.diff then return "]c" end
+        vim.schedule(function() gs.next_hunk() end)
+        return "<Ignore>"
+      end, { expr = true, desc = "Next Hunk" })
+
+      map("n", "[c", function()
+        if vim.wo.diff then return "[c" end
+        vim.schedule(function() gs.prev_hunk() end)
+        return "<Ignore>"
+      end, { expr = true, desc = "Prev Hunk" })
+
+      map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview Hunk" })
+      map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, { desc = "Blame Line" })
+      map("n", "<leader>hd", gs.diffthis, { desc = "Diff This" })
+    end,
+  },
+}
