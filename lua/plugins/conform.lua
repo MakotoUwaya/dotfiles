9d0b12b604f96ac0d73006d6d436244c710e5988
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2025-12-24 16:27:24 +0900

    🔧 Fix InsertLeave event
    - Auto format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,5 @@
 return {
   "stevearc/conform.nvim",
-  -- InsertLeave でも実行するため、イベントに InsertLeave を追加
   event = { "BufWritePre", "InsertLeave" },
   cmd = { "ConformInfo" },
   keys = {
@@ -26,27 +25,24 @@ return {
       xml = { "xmlformatter" },
       xaml = { "xmlformatter" },
     },
-    -- 保存時のオートフォーマット設定（既存）
     format_on_save = {
       timeout_ms = 500,
       lsp_fallback = true,
     },
   },
-  -- config 関数を使って InsertLeave の挙動を追加
   config = function(_, opts)
     local conform = require("conform")
     conform.setup(opts)
 
-    -- INSERT モードを抜けた時にフォーマットを実行する autocmd
     vim.api.nvim_create_autocmd("InsertLeave", {
       pattern = "*",
-      callback = function()
+      callback = function(args)
         conform.format({
+          bufnr = args.buf,
           lsp_fallback = true,
-          async = true, -- 入力直後のため、非同期で実行して操作を妨げないようにする
+          async = true,
         })
       end,
     })
   end,
 }
-
