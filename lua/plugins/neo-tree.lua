b52902fd3bd2d526d90571bbf40b8e9252b887ce
oichan <makoto.uwaya@gmail.com>
2026-01-03 20:01:53 +0900

    🔧 Update Neo-Tree settings

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,13 +7,19 @@ return {
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
-    lazy = false, -- neo-tree will lazily load itself
+    lazy = false,                    -- neo-tree will lazily load itself
     ---@module 'neo-tree'
     ---@type neotree.Config
     opts = {
       -- options go here
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          never_show = {
+            ".git",
+          },
+        },
+      },
     }
   }
 }
-
-
