1a0ea38a9e5715a41a6e2964e28c8712bcf7515b
oichan <makoto.uwaya@gmail.com>
2026-01-11 10:01:40 +0900

    🔧 Update keymaps for window edit

--- init.lua
+++ init.lua
@@ -3,10 +3,16 @@ require("config.lazy")
 vim.opt.number = true
 vim.opt.wrap = false
 
--- ウィンドウ移動のショートカット
 local keymap = vim.keymap
 
-keymap.set('n', '<C-h>', '<C-w>h', { desc = '左のウィンドウに移動' })
-keymap.set('n', '<C-j>', '<C-w>j', { desc = '下のウィンドウに移動' })
-keymap.set('n', '<C-k>', '<C-w>k', { desc = '上のウィンドウに移動' })
-keymap.set('n', '<C-l>', '<C-w>l', { desc = '右のウィンドウに移動' })
+-- window
+keymap.set('n', '<leader>sh', ':split<Return><C-w>w', { desc = 'Splits horizontal', noremap = true })
+keymap.set('n', '<leader>sv', ':vsplit<Return><C-w>w', { desc = 'Splits vertical', noremap = true })
+keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Navigate left' })
+keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Navigate down' })
+keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Navigate up' })
+keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Navigate right' })
+keymap.set("n", "<C-Up>", ":resize -3<CR>")
+keymap.set("n", "<C-Down>", ":resize +3<CR>")
+keymap.set("n", "<C-Left>", ":vertical resize -3<CR>")
+keymap.set("n", "<C-Right>", ":vertical resize +3<CR>")
