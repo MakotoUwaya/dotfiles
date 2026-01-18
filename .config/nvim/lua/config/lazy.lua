2c48a35ea89c7629f2f995bc17003912d53d0d13
oichan <makoto.uwaya@gmail.com>
2026-01-18 10:48:39 +0900

    🔨 Add Neovim quick quit script

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -23,6 +23,24 @@ vim.g.maplocalleader = '\\'
 
 vim.g.editorconfig = true
 
+-- Quick Quit
+-- https://zenn.dev/vim_jp/articles/ff6cd224fab0c7
+vim.api.nvim_create_autocmd('QuitPre', {
+  callback = function()
+    local current_win = vim.api.nvim_get_current_win()
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      if win ~= current_win then
+        local buf = vim.api.nvim_win_get_buf(win)
+        if vim.bo[buf].buftype == '' then
+          return
+        end
+      end
+    end
+    vim.cmd.only({ bang = true })
+  end,
+  desc = 'Close all special buffers and quit Neovim',
+})
+
 -- Setup lazy.nvim
 require('lazy').setup({
   spec = {
