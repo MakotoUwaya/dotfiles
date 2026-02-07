5a4fcb99d5069dbf24a953b0e934bfc65e8218ae
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2026-02-07 19:25:57 +0900

    🚚 Rename snaks.nvim

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -4,11 +4,7 @@ return {
   priority = 1000,
   ---@type snacks.Config
   opts = {
-    lazygit = {
-      -- your lazygit configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
+    lazygit = {},
   },
   keys = {
     { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
